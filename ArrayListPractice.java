d71dae5196d0ae7128bb1704e152879e20f76779
zakiahmed30 <53843840+zakiahmed30@users.noreply.github.com>
2019-08-27 18:20:05 -0400

    Add files via upload

---
+++ ArrayListPractice.java
@@ -0,0 +1,19 @@
+package execution;
+
+import java.util.Scanner;
+
+public class ArrayListPractice {
+    public static void main(String[] args) {
+        String[] students = {"Mark", "Jason", "Jen", "Ingrid"};
+        String[] subjects = {"Math", "Biology", "History", "Spanish"};
+        String[] grades = {"90", "88", "75", "85"};
+
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+
+
+
+
+
+    }
+}
